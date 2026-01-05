dd2cccaf2515d0a4ab3baee2f8decfc9941c28d4
Hosea54 <ejohhoseamwongwe@gmail.com>
2026-01-05 18:57:15 -0500

    update profile backend complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.listen(5000, () => {
   console.log("server is running on port" + PORT);
