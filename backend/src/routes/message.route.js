a52ec2bcb5d546ed5990a6121f3cd13686568ab0
Hosea54 <ejohhoseamwongwe@gmail.com>
2026-01-06 11:26:43 -0500

    get users for sidebar fuction created

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware";
+import { getUsersForSidebar } from "../controllers/message.controller.js";
+const router = express.Router();
+
+router.get("/users", protectRoute, getUsersForSidebar);
