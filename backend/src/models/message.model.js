f164666f1160497843d8d9bfb1f3c477da6326ed
Hosea54 <ejohhoseamwongwe@gmail.com>
2026-01-06 11:17:44 -0500

    message model complete

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+const messageSchema = new mongoose.Schema(
+  {
+    SenderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
