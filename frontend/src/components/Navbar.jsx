1ff6c0dab7e60531811a52f0ed731cbad4de94ba
Hosea54 <ejohhoseamwongwe@gmail.com>
2026-01-06 17:34:06 -0500

    zustand setup

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,9 @@
+import { useAuthStore } from "../store/useAuthStore";
+
 const Navbar = () => {
+  const { authUser } = useAuthStore();
+  console.log(authUser);
+
   return <div>Navbar</div>;
 };
 export default Navbar;
