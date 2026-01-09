3d6caa0fcfdec6d40cdcc13b2bf3d6823e8e9af0
Hosea54 <ejohhoseamwongwe@gmail.com>
2026-01-09 11:02:20 -0500

    sidebar component Ui and logic added

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(
   })
 );
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 app.listen(5000, () => {
   console.log("server is running on port" + PORT);
   connectDB();
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,4 +2,36 @@ import toast from "react-hot-toast";
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore
+export const useChatStore = create((set) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/users");
+      set({ users: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+
+  getMessages: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+  //todo: optimaize  later
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
+}));
